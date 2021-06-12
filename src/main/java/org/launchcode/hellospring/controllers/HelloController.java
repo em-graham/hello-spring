8f19c852add50ec1e1581a2f542e8fe4994fd085
Emilee Graham <emgraham@unknown.home>
2021-06-12 14:10:47 +0100

    Completed Spring Part 7: Class Annotations video

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -3,40 +3,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
+@RequestMapping("hello") // every single method should begin with /hello
 public class HelloController {
 
-    // Handles request at localhost:8080/hello
+    // Handles request at localhost:8080/hello/hello
 //    @GetMapping("hello")
-//    @ResponseBody
 //    public String hello() {
 //        return "Hello, Spring!";
 //    }
 
-    // Handles request at localhost:8080/goodbye
+    // Handles request at localhost:8080/hello/goodbye
     @GetMapping("goodbye")
-    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
-    // Handles requests localhost:8080/hello?name=LaunchCode
+    // Handles requests localhost:8080/hello/hello?name=LaunchCode
     // @GetMapping("hello") only accepts GET requests
-    @RequestMapping(method={RequestMethod.GET, RequestMethod.POST}, value = "hello") // accepts both GET and POST requests
-    @ResponseBody
+    @RequestMapping(method={RequestMethod.GET, RequestMethod.POST}) // accepts both GET and POST requests
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
-    // Handles requests localhost:8080/hello/LaunchCode
-    @GetMapping("hello/{name}")
-    @ResponseBody
+    // Handles requests localhost:8080/hello/hello/LaunchCode
+    @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
     // Form that allows user to type in name and greet them with that data
+    // hello/form
     @GetMapping("form")
-    @ResponseBody
     public String helloForm() {
         return "<html>" +
                 "<body>" +
