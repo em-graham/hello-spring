a5eb68ec7235515dc6afe27fd58093a8398dabc8
Emilee Graham <emgraham@unknown.home>
2021-06-12 13:48:40 +0100

    Completed Spring Part 4: Query Param video

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -6,16 +6,24 @@ import org.springframework.web.bind.annotation.*;
 public class HelloController {
 
     // Handles request at localhost:8080/hello
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello, Spring!";
-    }
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
 
+    // Handles request at localhost:8080/goodbye
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
+    }
+
+    // Handles requests /hello?name=LaunchCode
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
 
 }
